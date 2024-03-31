9e8d32d9b5c3e1bb3fe3c1534d556f77259563a8
Bilal Bashir <164682514+bilalbashir08@users.noreply.github.com>
2024-03-31 20:16:23 +0500

    Update mapper4.py

--- mapper4.py
+++ mapper4.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 import sys
+import string
+from collections import defaultdict
+
+def hash_code(word):
+    return abs(hash(word)) % (10 ** 6)
+
+def load_idf(code_file):
+    idf = {}
+    with open(code_file, "r") as file:
+        for line in file:
+            code, df = line.strip().split('\t')
+            idf[int(code)] = int(df)
+    return idf
 
 def mapper():
-    current_article = None
-    current_counts = {}
+    idf = load_idf('/home/bilal/output4.txt')
+    article_dict = defaultdict(int)
 
     for line in sys.stdin:
         line = line.strip()
+        words = line.lower().translate(str.maketrans('', '', string.punctuation)).split()
+        for word in words:
+            code = hash_code(word)
+            if code:
+                article_dict[code] += 1
+    
+    for code,value in article_dict.items():
+        idf_val = idf.get(code)
+        if idf_val and idf_val!=0:
+            article_dict[code] = value / idf_val
+        else:
+            article_dict[code] = value
 
-        if line.isdigit():  # Start of a new article block detection
-            if current_article:
-                print_article(current_article, current_counts)
-            current_article = int(line)
-            current_counts.clear()
-            continue
-
-        # Interpret lines as tuples (code, count)
-        code, count = map(int, line.strip('()').split(', '))
-        current_counts[code] = current_counts.get(code, 0) + count
-
-    # Output the last article's counts
-    if current_article:
-        print_article(current_article, current_counts)
-
-def print_article(article_id, counts):
-    # Format counts dictionary into string
-    counts_str = ', '.join(f"{code}:{count}" for code, count in counts.items())
-    print(f"{article_id}{{{counts_str}}}")
+        print(f'{code}\t{article_dict[code]}')
 
 if __name__ == "__main__":
     mapper()
+
+
 
