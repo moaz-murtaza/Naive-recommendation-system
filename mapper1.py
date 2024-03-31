97e019f64dc47eb78541fb1508fbefaba9c291a1
Bilal Bashir <164682514+bilalbashir08@users.noreply.github.com>
2024-03-31 19:30:37 +0500

    Update mapper1.py

--- mapper1.py
+++ mapper1.py
@@ -1,44 +1,30 @@
 #!/usr/bin/env python3
 import sys
 import string
-import csv
 
-def load_codes(code_file):
-    codes = {}
-    with open(code_file, "r") as file:
-        for line in file:
-            code, word = line.strip().split('\t')
-            codes[word] = int(code)
-    return codes
+def hash_code(word):
+    return abs(hash(word)) % (10 ** 6)
 
 def mapper():
-    codes = load_codes('/home/moaz/output1.txt')
-    articles = dict()
 
-    lines = csv.reader(sys.stdin)
-    
-    next(lines)
-
-    for columns in lines:
+    next(sys.stdin)
+    for line in sys.stdin:
+        columns = line.strip().split(',')
+        # If there are less than 4 columns, skip this line
         if len(columns) < 4:
             continue
 
-        words = columns[3].lower().translate(str.maketrans('', '', string.punctuation)).split() 
-        article_id = columns[0]
+        words = columns[3].lower().translate(str.maketrans('', '', string.punctuation)).split()
+        article_dict = {}
 
-        if not articles.get(article_id):
-            articles[article_id] = dict.fromkeys(codes.values(), 0)
-
-        article_dict = articles[article_id]
-        
         for word in words:
-            code = codes.get(word)
-            if code:
+            code = hash_code(word)
+            if code not in article_dict:
+                article_dict[code] = 1
+            else:
                 article_dict[code] += 1
 
-    # Print results for all articles
-    for article_id, word_frequencies in articles.items():
-        print(f"{article_id}{word_frequencies}") 
+        print(f"{columns[0]}{article_dict}")
 
 if __name__ == "__main__":
     mapper()
